46e4f083247ac30ea8608c35f547423f70169924
leonid-tankov <tankov.lenya@mail.ru>
2023-05-22 11:43:14 +0300

    HW2 is completed

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,56 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"log"
+	"strconv"
+	"strings"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+func Unpack(str string) (string, error) {
+	var result strings.Builder
+	if str == "" {
+		return "", nil
+	}
+	if isDigit(rune(str[0])) {
+		return "", ErrInvalidString
+	}
+	var prev rune
+	for _, s := range str {
+		if prev == 0 {
+			prev = s
+			continue
+		}
+		if isDigit(s) && isDigit(prev) {
+			return "", ErrInvalidString
+		}
+		if isDigit(s) {
+			repeat, err := strconv.Atoi(string(s))
+			if err != nil {
+				log.Println(err)
+				return "", ErrInvalidString
+			}
+			result.WriteString(strings.Repeat(string(prev), repeat))
+			prev = s
+		} else {
+			if isDigit(prev) {
+				prev = s
+				continue
+			}
+			result.WriteRune(prev)
+			prev = s
+		}
+	}
+	if !isDigit(prev) {
+		result.WriteRune(prev)
+	}
+	return result.String(), nil
+}
+
+func isDigit(symbol rune) bool {
+	if symbol >= 48 && symbol <= 57 {
+		return true
+	}
+	return false
 }
--- hw02_unpack_string/unpack_test.go
+++ hw02_unpack_string/unpack_test.go
@@ -16,6 +16,12 @@ func TestUnpack(t *testing.T) {
 		{input: "abccd", expected: "abccd"},
 		{input: "", expected: ""},
 		{input: "aaa0b", expected: "aab"},
+		{input: "d\n5abc", expected: "d\n\n\n\n\nabc"},
+		{input: "dabc3", expected: "dabccc"},
+		{input: "dabc0", expected: "dab"},
+		{input: "d0", expected: ""},
+		{input: "d3", expected: "ddd"},
+		{input: "н3лорим2к", expected: "нннлориммк"},
 		// uncomment if task with asterisk completed
 		// {input: `qwe\4\5`, expected: `qwe45`},
 		// {input: `qwe\45`, expected: `qwe44444`},
