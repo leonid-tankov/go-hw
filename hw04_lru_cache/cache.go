5519b01bcab4cdd353cc6782e9c35ba7db039b05
leonid-tankov <tankov.lenya@mail.ru>
2023-07-03 10:10:57 +0300

    HW4 is completed

--- hw04_lru_cache/cache.go
+++ hw04_lru_cache/cache.go
@@ -1,6 +1,13 @@
 package hw04lrucache
 
+import "sync"
+
 type Key string
+
+type Element struct {
+	Key   Key
+	Value interface{}
+}
 
 type Cache interface {
 	Set(key Key, value interface{}) bool
@@ -9,8 +16,7 @@ type Cache interface {
 }
 
 type lruCache struct {
-	Cache // Remove me after realization.
-
+	sync.Mutex
 	capacity int
 	queue    List
 	items    map[Key]*ListItem
@@ -21,5 +27,54 @@ func NewCache(capacity int) Cache {
 		capacity: capacity,
 		queue:    NewList(),
 		items:    make(map[Key]*ListItem, capacity),
+	}
+}
+
+func (lru *lruCache) Set(key Key, value interface{}) bool {
+	lru.Lock()
+	defer lru.Unlock()
+	if item, exist := lru.items[key]; exist {
+		lru.queue.MoveToFront(item)
+		item.Value.(*Element).Value = value
+		return true
+	}
+
+	if lru.queue.Len() == lru.capacity {
+		lru.clearLast()
+	}
+
+	element := &Element{
+		Key:   key,
+		Value: value,
+	}
+	item := lru.queue.PushFront(element)
+	lru.items[key] = item
+
+	return false
+}
+
+func (lru *lruCache) Get(key Key) (interface{}, bool) {
+	lru.Lock()
+	defer lru.Unlock()
+	item, exist := lru.items[key]
+	if !exist {
+		return nil, false
+	}
+	lru.queue.MoveToFront(item)
+
+	return item.Value.(*Element).Value, true
+}
+
+func (lru *lruCache) Clear() {
+	lru.Lock()
+	defer lru.Unlock()
+	lru.items = make(map[Key]*ListItem, lru.capacity)
+	lru.queue = NewList()
+}
+
+func (lru *lruCache) clearLast() {
+	if item := lru.queue.Back(); item != nil {
+		lru.queue.Remove(item)
+		delete(lru.items, item.Value.(*Element).Key)
 	}
 }
