70061ab37ca96289b441431576da1000d8fa3b6a
leonid-tankov <34457362+leonid-tankov@users.noreply.github.com>
2023-09-13 17:21:58 +0300

    HW7 is completed (#7)
    * HW7 is completed

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -2,14 +2,102 @@ package main
 
 import (
 	"errors"
+	"fmt"
+	"io"
+	"os"
+	"strings"
 )
 
 var (
 	ErrUnsupportedFile       = errors.New("unsupported file")
 	ErrOffsetExceedsFileSize = errors.New("offset exceeds file size")
+	ErrNegativeArgument      = errors.New("one of the arguments is negative")
+	ErrRootWriting           = errors.New("attempt to write to the root folder")
+	ErrSamePath              = errors.New("to and from paths are the same")
 )
 
 func Copy(fromPath, toPath string, offset, limit int64) error {
-	// Place your code here.
+	if offset < 0 || limit < 0 {
+		return ErrNegativeArgument
+	}
+
+	if strings.HasPrefix(toPath, "/root") {
+		return ErrRootWriting
+	}
+
+	if fromPath == toPath {
+		return ErrSamePath
+	}
+
+	fileInfo, err := os.Stat(fromPath)
+	if err != nil {
+		return err
+	}
+
+	if !fileInfo.Mode().IsRegular() {
+		return ErrUnsupportedFile
+	}
+	fileSize := fileInfo.Size()
+	if fileSize < offset {
+		return ErrOffsetExceedsFileSize
+	}
+
+	src, err := os.OpenFile(fromPath, os.O_RDONLY, 0o444)
+	if err != nil {
+		return err
+	}
+	defer src.Close()
+
+	dst, err := os.OpenFile(toPath, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0o666)
+	if err != nil {
+		return err
+	}
+	defer dst.Close()
+
+	_, err = src.Seek(offset, io.SeekStart)
+	if err != nil {
+		return err
+	}
+	if (limit > fileSize-offset) || (limit == 0) {
+		limit = fileSize - offset
+	}
+	bSize := getBufSize(limit)
+	buf := make([]byte, bSize)
+	fmt.Println("Start copying")
+	for i := 1; i <= int(limit); i += bSize {
+		n, err := src.Read(buf)
+		if err != nil && err != io.EOF {
+			return err
+		}
+		if n == 0 {
+			break
+		}
+
+		if _, err := dst.Write(buf[:n]); err != nil {
+			return err
+		}
+		progressBytes := i
+		percent := 100 * float64(i) / float64(limit)
+		if i > int(limit)-bSize {
+			percent = 100
+			progressBytes = int(limit)
+		}
+		progress := strings.Repeat("#", int(percent)/2)
+		fmt.Printf("\r[%-50s]%5.1f%% %15d/%d bytes ", progress, percent, progressBytes, limit)
+	}
+	fmt.Println("\nComplete copying")
 	return nil
+}
+
+func getBufSize(limit int64) int {
+	switch {
+	case limit < 1048576: // < 1MB
+		return int(limit / 2)
+	case limit >= 1048576 && limit < 1073741824: // from 1MB to 1GB
+		return int(limit / 2000)
+	case limit >= 1073741824: // > 1GB
+		return int(limit / 2000000)
+	default:
+		return int(limit)
+	}
 }
--- hw07_file_copying/copy_test.go
+++ hw07_file_copying/copy_test.go
@@ -1,7 +1,90 @@
 package main
 
-import "testing"
+import (
+	"errors"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	"github.com/udhos/equalfile"
+)
 
 func TestCopy(t *testing.T) {
-	// Place your code here.
+	cmp := equalfile.New(nil, equalfile.Options{})
+
+	t.Run("non-existent from file", func(t *testing.T) {
+		err := Copy("testdata/input-222.txt", "output.txt", 0, 0)
+		require.Error(t, err, "no error %q", err)
+	})
+
+	t.Run("root writing", func(t *testing.T) {
+		err := Copy("testdata/input.txt", "/root/output.txt", 0, 0)
+		require.Truef(t, errors.Is(err, ErrRootWriting), "actual error %q", err)
+	})
+
+	t.Run("the same input and output", func(t *testing.T) {
+		err := Copy("testdata/input.txt", "testdata/input.txt", 0, 0)
+		require.Truef(t, errors.Is(err, ErrSamePath), "actual error %q", err)
+	})
+
+	t.Run("negative argument", func(t *testing.T) {
+		tmpFile, _ := os.CreateTemp("", "tmp_file.txt")
+		defer os.Remove(tmpFile.Name())
+		defer tmpFile.Close()
+
+		err := Copy("testdata/input.txt", tmpFile.Name(), -1, 0)
+		require.Truef(t, errors.Is(err, ErrNegativeArgument), "actual error %q", err)
+	})
+
+	t.Run("unsupported file", func(t *testing.T) {
+		tmpFile, _ := os.CreateTemp("", "tmp_file.txt")
+		defer os.Remove(tmpFile.Name())
+		defer tmpFile.Close()
+
+		err := Copy("testdata", tmpFile.Name(), 0, 0)
+		require.Truef(t, errors.Is(err, ErrUnsupportedFile), "actual error %q", err)
+	})
+
+	t.Run("offset exceeds file size", func(t *testing.T) {
+		tmpFile, _ := os.CreateTemp("", "tmp_file.txt")
+		defer os.Remove(tmpFile.Name())
+		defer tmpFile.Close()
+
+		err := Copy("testdata/input.txt", tmpFile.Name(), 10000, 0)
+		require.Truef(t, errors.Is(err, ErrOffsetExceedsFileSize), "actual error %q", err)
+	})
+
+	t.Run("files in testdata", func(t *testing.T) {
+		tmpFile, _ := os.CreateTemp("", "tmp_file.txt")
+		defer os.Remove(tmpFile.Name())
+		defer tmpFile.Close()
+
+		require.NoError(t, Copy("testdata/input.txt", tmpFile.Name(), 0, 0), "failed copy file")
+		equal, _ := cmp.CompareFile(tmpFile.Name(), "testdata/out_offset0_limit0.txt")
+		require.Truef(t, equal, "files not equal")
+
+		require.NoError(t, Copy("testdata/input.txt", tmpFile.Name(), 0, 10), "failed copy file")
+		equal, _ = cmp.CompareFile(tmpFile.Name(), "testdata/out_offset0_limit10.txt")
+		require.Truef(t, equal, "files not equal")
+
+		require.NoError(t, Copy("testdata/input.txt", tmpFile.Name(), 0, 1000), "failed copy file")
+		equal, _ = cmp.CompareFile(tmpFile.Name(), "testdata/out_offset0_limit1000.txt")
+		require.Truef(t, equal, "files not equal")
+
+		require.NoError(t, Copy("testdata/input.txt", tmpFile.Name(), 0, 10000), "failed copy file")
+		equal, _ = cmp.CompareFile(tmpFile.Name(), "testdata/out_offset0_limit10000.txt")
+		require.Truef(t, equal, "files not equal")
+
+		require.NoError(t, Copy("testdata/input.txt", tmpFile.Name(), 10, 0), "failed copy file")
+		equal, _ = cmp.CompareFile(tmpFile.Name(), "testdata/out_offset10_limit0.txt")
+		require.Truef(t, equal, "files not equal")
+
+		require.NoError(t, Copy("testdata/input.txt", tmpFile.Name(), 100, 1000), "failed copy file")
+		equal, _ = cmp.CompareFile(tmpFile.Name(), "testdata/out_offset100_limit1000.txt")
+		require.Truef(t, equal, "files not equal")
+
+		require.NoError(t, Copy("testdata/input.txt", tmpFile.Name(), 6000, 1000), "failed copy file")
+		equal, _ = cmp.CompareFile(tmpFile.Name(), "testdata/out_offset6000_limit1000.txt")
+		require.Truef(t, equal, "files not equal")
+	})
 }
